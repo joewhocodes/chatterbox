6460134aea1e53fcec1b75548cc72addf0082abf
Joseph Ulyatt <joeulyatt1@hotmail.co.uk>
2023-06-11 17:31:52 +0200

    reformat code

--- screens/Login.js
+++ screens/Login.js
@@ -1,116 +1,140 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, Image, SafeAreaView, TouchableOpacity, StatusBar, Alert } from "react-native";
-import { Input } from 'react-native-elements';
-import { auth } from '../firebase';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+  StatusBar,
+  Alert,
+} from 'react-native';
+import {Input} from 'react-native-elements';
+import {auth} from '../firebase';
+import {signInWithEmailAndPassword} from 'firebase/auth';
 import Icon from 'react-native-vector-icons/FontAwesome';
-const backImage = require("../assets/backImage.png");
+const backImage = require('../assets/backImage.png');
 
-const Login = ({ navigation }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+const Login = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleSignin = () => {
-        signInWithEmailAndPassword(auth, email, password)
-          .then((userCredential) => {
-            navigation.navigate('Chat');
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            alert(errorMessage);
-          });
-      };
+  const handleSignin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(userCredential => {
+        navigation.navigate('Chat');
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
+  };
 
-      return (
-        <View style={styles.container}>
-          <Image source={backImage} style={styles.backImage} />
-          <View style={styles.whiteSheet} />
-          <SafeAreaView style={styles.form}>
-            <Text style={styles.title}>Log In</Text>
-             <TextInput
-            style={styles.input}
-            placeholder="Enter email"
-            autoCapitalize="none"
-            keyboardType="email-address"
-            textContentType="emailAddress"
-            autoFocus={true}
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Enter password"
-            autoCapitalize="none"
-            autoCorrect={false}
-            secureTextEntry={true}
-            textContentType="password"
-            value={password}
-            onChangeText={(text) => setPassword(text)}
-          />
-          <TouchableOpacity style={styles.button} onPress={handleSignin}>
-            <Text style={{fontWeight: 'bold', color: '#fff', fontSize: 18}}> Log In</Text>
+  return (
+    <View style={styles.container}>
+      <Image source={backImage} style={styles.backImage} />
+      <View style={styles.whiteSheet} />
+      <SafeAreaView style={styles.form}>
+        <Text style={styles.title}>Log In</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter email"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+          autoFocus={true}
+          value={email}
+          onChangeText={text => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry={true}
+          textContentType="password"
+          value={password}
+          onChangeText={text => setPassword(text)}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleSignin}>
+          <Text style={{fontWeight: 'bold', color: '#fff', fontSize: 18}}>
+            {' '}
+            Log In
+          </Text>
+        </TouchableOpacity>
+        <View
+          style={{
+            marginTop: 20,
+            flexDirection: 'row',
+            alignItems: 'center',
+            alignSelf: 'center',
+          }}>
+          <Text style={{color: 'gray', fontWeight: '600', fontSize: 14}}>
+            Don't have an account?{' '}
+          </Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
+            <Text style={{color: '#f57c00', fontWeight: '600', fontSize: 14}}>
+              {' '}
+              Sign Up
+            </Text>
           </TouchableOpacity>
-          <View style={{marginTop: 20, flexDirection: 'row', alignItems: 'center', alignSelf: 'center'}}>
-            <Text style={{color: 'gray', fontWeight: '600', fontSize: 14}}>Don't have an account? </Text>
-            <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
-              <Text style={{color: '#f57c00', fontWeight: '600', fontSize: 14}}> Sign Up</Text>
-            </TouchableOpacity>
-          </View>
-          </SafeAreaView>
-          <StatusBar barStyle="light-content" />
         </View>
-      );
-    }
+      </SafeAreaView>
+      <StatusBar barStyle="light-content" />
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-    },
-    title: {
-      fontSize: 36,
-      fontWeight: 'bold',
-      color: "orange",
-      alignSelf: "center",
-      paddingBottom: 24,
-    },
-    input: {
-      backgroundColor: "#F6F7FB",
-      height: 58,
-      marginBottom: 20,
-      fontSize: 16,
-      borderRadius: 10,
-      padding: 12,
-    },
-    backImage: {
-      width: "100%",
-      height: 340,
-      position: "absolute",
-      top: 0,
-      resizeMode: 'cover',
-    },
-    whiteSheet: {
-      width: '100%',
-      height: '75%',
-      position: "absolute",
-      bottom: 0,
-      backgroundColor: '#fff',
-      borderTopLeftRadius: 60,
-    },
-    form: {
-      flex: 1,
-      justifyContent: 'center',
-      marginHorizontal: 30,
-    },
-    button: {
-      backgroundColor: '#f57c00',
-      height: 58,
-      borderRadius: 10,
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 40,
-    },
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: 'orange',
+    alignSelf: 'center',
+    paddingBottom: 24,
+  },
+  input: {
+    backgroundColor: '#F6F7FB',
+    height: 58,
+    marginBottom: 20,
+    fontSize: 16,
+    borderRadius: 10,
+    padding: 12,
+  },
+  backImage: {
+    width: '100%',
+    height: 340,
+    position: 'absolute',
+    top: 0,
+    resizeMode: 'cover',
+  },
+  whiteSheet: {
+    width: '100%',
+    height: '75%',
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 60,
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 30,
+  },
+  button: {
+    backgroundColor: '#f57c00',
+    height: 58,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+});
 
 export default Login;
