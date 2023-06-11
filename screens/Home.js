0a1eef7c0c69d256e048acbb8788e011dcdbc37a
Joseph Ulyatt <joeulyatt1@hotmail.co.uk>
2023-06-11 17:15:18 +0200

    install extra fontawesome icons

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,7 @@
 import React, { useEffect } from "react";
 import { View, TouchableOpacity, Text, Image, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-// import { FontAwesome } from '@expo/vector-icons';
 import colors from '../colors';
-// import { Entypo } from '@expo/vector-icons';
-const catImageUrl = "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d";
 import { faMugSaucer } from '@fortawesome/free-solid-svg-icons/faMugSaucer'
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 
@@ -15,18 +12,18 @@ const Home = () => {
     useEffect(() => {
         navigation.setOptions({
             headerLeft: () => (
-                <FontAwesomeIcon icon={faMugSaucer} size={24} color={colors.gray} style={{marginLeft: 15}}/>
+                <FontAwesomeIcon icon="fa-solid fa-right-from-bracket" />
             ),
-            headerRight: () => (
-                <Image
-                    source={{ uri: catImageUrl }}
-                    style={{
-                        width: 40,
-                        height: 40,
-                        marginRight: 15,
-                    }}
-                />
-            ),
+            // headerRight: () => (
+            //     <Image
+            //         source={}
+            //         style={{
+            //             width: 40,
+            //             height: 40,
+            //             marginRight: 15,
+            //         }}
+            //     />
+            // ),
         });
     }, [navigation]);
 
