f9c176ffa666c0a93bc55630a426222e169b2883
Joseph Ulyatt <joeulyatt1@hotmail.co.uk>
2023-06-11 17:04:52 +0200

    fix rnsvgp error by installing react svg

--- screens/Home.js
+++ screens/Home.js
@@ -5,6 +5,8 @@ import { useNavigation } from "@react-navigation/native";
 import colors from '../colors';
 // import { Entypo } from '@expo/vector-icons';
 const catImageUrl = "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d";
+import { faMugSaucer } from '@fortawesome/free-solid-svg-icons/faMugSaucer'
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 
 const Home = () => {
 
@@ -12,9 +14,9 @@ const Home = () => {
 
     useEffect(() => {
         navigation.setOptions({
-            // headerLeft: () => (
-            //     <FontAwesome name="search" size={24} color={colors.gray} style={{marginLeft: 15}}/>
-            // ),
+            headerLeft: () => (
+                <FontAwesomeIcon icon={faMugSaucer} size={24} color={colors.gray} style={{marginLeft: 15}}/>
+            ),
             headerRight: () => (
                 <Image
                     source={{ uri: catImageUrl }}
